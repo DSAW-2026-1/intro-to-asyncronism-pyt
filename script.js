3df7670981c66661162b249db0c533e7fb0819df
llotoa64 <115710105+llotoa64@users.noreply.github.com>
2026-04-02 11:51:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,28 @@ async function buscarPokemon() {
 
     try {
         const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
-        const data = await respuesta.json();
 
+        if (!respuesta.ok) {
+            throw new Error("No encontrado");
+        }
+
+        const data = await respuesta.json();
         mostrarPokemon(data);
 
     } catch (error) {
-        alert("Pokémon no encontrado ❌");
+        document.getElementById("resultado").innerHTML = `
+            <p style="color: red;">Pokémon not found ❌</p>
+        `;
     }
+}
+
+async function pokemonRandom() {
+    const random = Math.floor(Math.random() * 151) + 1;
+
+    const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${random}`);
+    const data = await respuesta.json();
+
+    mostrarPokemon(data);
 }
 
 function mostrarPokemon(pokemon) {
@@ -24,9 +39,15 @@ function mostrarPokemon(pokemon) {
 
     resultado.innerHTML = `
         <h2>${pokemon.name.toUpperCase()}</h2>
-        <img src="${pokemon.sprites.front_default}">
+        <img src="${pokemon.sprites.other['official-artwork'].front_default}">
         <p><strong>Weight:</strong> ${pokemon.weight}</p>
         <p><strong>Type:</strong> ${types}</p>
         <p><strong>Abilities:</strong> ${abilities}</p>
     `;
 }
+
+document.getElementById("pokemonInput").addEventListener("keypress", function(e) {
+    if (e.key === "Enter") {
+        buscarPokemon();
+    }
+});
