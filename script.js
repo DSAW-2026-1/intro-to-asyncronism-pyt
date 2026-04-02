4738fa73698672822d78f0219a38815415b82ca9
llotoa64 <115710105+llotoa64@users.noreply.github.com>
2026-04-02 11:23:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const boton = document.getElementById("btnBuscar");
 
-boton.addEventListener("click", function() {
-    alert("Todo funciona 🔥");
-});
+boton.addEventListener("click", buscarPokemon);
+
+async function buscarPokemon() {
+    const nombre = document.getElementById("pokemonInput").value.toLowerCase();
+
+    try {
+        const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
+        const data = await respuesta.json();
+
+        mostrarPokemon(data);
+
+    } catch (error) {
+        alert("Pokémon no encontrado ❌");
+    }
+}
+
+function mostrarPokemon(pokemon) {
+    const resultado = document.getElementById("resultado");
+
+    resultado.innerHTML = `
+        <h2>${pokemon.name}</h2>
+        <p>Peso: ${pokemon.weight}</p>
+        <img src="${pokemon.sprites.front_default}">
+    `;
+}
